39d7dfc820ab2c6a9c3ef219f32cfa4a269a8e3b
Oleg <57754088+OlegPozhenskij@users.noreply.github.com>
2023-02-08 21:30:46 +0400

    Add MVC model, test fxml, base jfx settings in Mainframe

---
+++ src/main/scala/MVC/View.scala
@@ -0,0 +1,5 @@
+package MVC
+
+class View {
+
+}
---
+++ src/main/scala/Mainframe.scala
@@ -0,0 +1,27 @@
+import javafx.application.Application
+import javafx.application.Application.launch
+import javafx.fxml.FXMLLoader
+import javafx.scene.Scene
+import javafx.stage.Stage
+
+//класс запуска UI
+class Mainframe extends Application {
+  override def start(primaryStage: Stage) {
+    // url ресурса (файла)
+    val cl = getClass.getResource("test.fxml")
+
+    //Specify the scene to be used on this stage.
+    primaryStage.setScene(new Scene(FXMLLoader.load(cl)))
+
+    primaryStage.setTitle("Title")
+
+//    Attempts to show this Window by setting visibility to true
+    primaryStage.show()
+  }
+}
+
+object Mainframe extends App {
+//  Launch a standalone application. This method is typically called from the main method().
+  launch(classOf[Mainframe], args: _*)
+}
+
