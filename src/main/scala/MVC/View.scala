2b7fedce0f0dbd3199c6baaac682a8d5c144d721
Oleg <57754088+OlegPozhenskij@users.noreply.github.com>
2023-02-10 13:33:32 +0400

    Download sceneBuilder, add xhml structure, add simpel view

--- src/main/scala/MVC/View.scala
+++ src/main/scala/MVC/View.scala
@@ -1,5 +1,23 @@
 package MVC
 
+import javafx.event.ActionEvent
+import javafx.fxml.FXML
+import javafx.scene.control.{Button, TextArea}
+
 class View {
+
+  //Компоненты соединённые с файлом .fxml
+  @FXML
+  var msgField: TextArea = _
+  var enterMsg: Button = _
+
+  // При событии нажаия на кнопу отправляем запускаем метод
+  def sendMsg(actionEvent: ActionEvent): Unit = {
+    println("New msg: " + msgField.getText)
+  }
+
+}
+
+object View {
 
 }
